88760d4c4af3d7289d934174a1fc8f6e7ee7c3ef
David Usov <usovd660@sus-am-bktm.de>
2023-06-09 10:55:42 +0200

    added my projects and finished the site

--- views/js/mainpage.js
+++ views/js/mainpage.js
@@ -8,7 +8,8 @@ function reveal() {
 
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
-        } else {
+        }
+        else {
             reveals[i].classList.remove("active");
         }
     }
@@ -29,12 +30,9 @@ function reveal2() {
         }
     }
 }
-window.addEventListener("scroll", reveal)
-window.addEventListener("scroll", reveal2)
 
 function reveal0() {
     const reveals = document.querySelectorAll(".reveal0");
-
     for (let k = 0; k < reveals.length;k++) {
         const windowHeight = window.innerHeight;
         const elementTop = reveals[k].getBoundingClientRect().top;
@@ -48,5 +46,6 @@ function reveal0() {
         }
     }
 }
-
+window.addEventListener("scroll", reveal)
+window.addEventListener("scroll", reveal2)
 window.addEventListener("load", reveal0);
